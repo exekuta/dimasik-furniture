60ee380a03301f0f087e6a6d2583723d589c91a5
unknown <exekuta@gmail.com>
2022-12-13 00:25:45 +0300

    fix: change main screen sizes

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -6,20 +6,23 @@ const Main = () => {
   return (
     <Box component="div" className="main-container">
       <Box component="div" className="main-text-container">
-        <Typography
-          sx={{ fontSize: '52px', fontWeight: '600', letterSpacing: '1px' }}
-          className="main-text-title"
-        >
-          Проектирование и сборка корпусной мебели с гарантией
-        </Typography>
-        <Typography
-          sx={{ fontSize: '22px', fontWeight: '300', lineHeight: '1.55' }}
-          component="span"
-          className="main-text-subtitle"
-        >
-          Мы занимаемся проектированием и сборкой корпусной кухонной мебели и
-          гарнитуров, гардеробных и шкафов-купе
-        </Typography>
+        <Box component="div" className="main-text-bg-cover"></Box>
+        <Box component="div" className ="main-text-wrapper">
+          <Typography
+            sx={{ fontSize: '52px', fontWeight: '600', letterSpacing: '1px' }}
+            className="main-text-title"
+          >
+            Проектирование и сборка корпусной мебели с гарантией
+          </Typography>
+          <Typography
+            sx={{ fontSize: '22px', fontWeight: '300', lineHeight: '1.55' }}
+            component="span"
+            className="main-text-subtitle"
+          >
+            Мы занимаемся проектированием и сборкой корпусной кухонной мебели и
+            гарнитуров, гардеробных и шкафов-купе
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
