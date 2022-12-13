3a6d7c9f1d32f689ef80735fe181e13e449a9449
exekuta <exekuta@gmail.com>
2022-12-13 16:57:25 +0300

    feat: add main buttons

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Services from './components/Services/Services';
+import { Box, CssBaseline } from '@mui/material';
 import './App.scss';
+import MuiTheme from './components/MuiTheme/MuiTheme';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Services />
-    </div>
+    <MuiTheme>
+      <React.Fragment>
+        <CssBaseline />
+        <Box component="div" className="App">
+          <Header />
+          <Main />
+          <Services />
+        </Box>
+      </React.Fragment>
+    </MuiTheme>
   );
 }
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import React from 'react';
 import './Main.scss';
 
@@ -6,22 +6,33 @@ const Main = () => {
   return (
     <Box component="div" className="main-container">
       <Box component="div" className="main-text-container">
-        <Box component="div" className="main-text-bg-cover"></Box>
-        <Box component="div" className ="main-text-wrapper">
-          <Typography
-            sx={{ fontSize: '52px', fontWeight: '600', letterSpacing: '1px' }}
-            className="main-text-title"
-          >
-            Проектирование и сборка корпусной мебели с гарантией
-          </Typography>
-          <Typography
-            sx={{ fontSize: '22px', fontWeight: '300', lineHeight: '1.55' }}
-            component="span"
-            className="main-text-subtitle"
-          >
-            Мы занимаемся проектированием и сборкой корпусной кухонной мебели и
-            гарнитуров, гардеробных и шкафов-купе
-          </Typography>
+        <Box component="div" className="main-text-bg-cover" />
+        <Box component="div" className="main-text-flex">
+          <Box component="div" className="main-text-wrapper">
+            <Typography
+              sx={{ fontSize: '52px', fontWeight: '600', letterSpacing: '1px' }}
+              className="main-text-title"
+            >
+              Проектирование и сборка корпусной мебели с гарантией
+            </Typography>
+            <Typography
+              sx={{ fontSize: '22px', fontWeight: '300', lineHeight: '1.55' }}
+              component="span"
+              className="main-text-subtitle"
+            >
+              Мы занимаемся проектированием и сборкой корпусной кухонной мебели и
+              гарнитуров, гардеробных и шкафов-купе
+            </Typography>
+            <Box component="div" className="main-text-buttons">
+              <Button variant="contained" className="button">
+                Рассчитать стоимость
+              </Button>
+              <Button variant="outlined" className="button" sx={{ color: '#fff', borderColor: '#fff'}}>
+                Услуги
+              </Button>
+            </Box>
+          </Box>
+          
         </Box>
       </Box>
     </Box>
