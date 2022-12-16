9efea56706cd02c55fdeb97fa92cdfdd68bc577d
exekuta <exekuta@gmail.com>
2022-12-16 17:00:15 +0300

    feat: start services section

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import './Services.scss';
 
 const Services = () => {
   return (
-    <div>
-      <h1>Services</h1>
-    </div>
+    <Box component="div" className="services-container" id="services">
+      <Box component="div" className="services-text-container">
+        <Typography
+          component="span"
+          sx={{ color: '#000', fontSize: '42px', fontWeight: '600', lineHeight: '1.23', marginBottom: '40px' }}
+          className="services-text-title"
+        >
+          Наши услуги
+        </Typography>
+        <Typography
+          component="span"
+          sx={{ color: '#000', fontSize: '22px', fontWeight: '300', lineHeight: '1.5', marginBottom: '105px'  }}
+          className="services-text-subtitle"
+        >
+          Мы занимаемся проектированием и сборкой
+          <br />
+          корпусной кухонной мебели и гарнитуров, 
+          <br />
+          гардеробных и шкафов-купе
+          <br />
+          НУЖНО ЕЩЕ ТЕКСТА!
+        </Typography>
+      </Box>
+      
+    </Box>
   )
 }
 
