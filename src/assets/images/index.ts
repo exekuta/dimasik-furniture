7cf6380dcaae96d3d08edf4f54c57035af982eba
exekuta <exekuta@gmail.com>
2022-12-20 16:55:13 +0300

    feat: add image list to portfolio

---
+++ src/assets/images/index.ts
@@ -0,0 +1,45 @@
+import portfolio01 from './portfolio01.jpg';
+import portfolio02 from './portfolio02.jpg';
+import portfolio03 from './portfolio03.jpg';
+import portfolio04 from './portfolio04.jpg';
+import portfolio05 from './portfolio05.jpg';
+import portfolio06 from './portfolio06.jpg';
+import portfolio07 from './portfolio07.jpg';
+import portfolio08 from './portfolio08.jpg';
+
+const itemData = [
+  {
+    img: `${portfolio01}`,
+    title: 'Portfolio 01',
+  },
+  {
+    img: `${portfolio02}`,
+    title: 'Portfolio 02',
+  },
+  {
+    img: `${portfolio03}`,
+    title: 'Portfolio 03',
+  },
+  {
+    img: `${portfolio04}`,
+    title: 'Portfolio 04',
+  },
+  {
+    img: `${portfolio05}`,
+    title: 'Portfolio 05',
+  },
+  {
+    img: `${portfolio06}`,
+    title: 'Portfolio 06',
+  },
+  {
+    img: `${portfolio07}`,
+    title: 'Portfolio 07',
+  },
+  {
+    img: `${portfolio08}`,
+    title: 'Portfolio 08',
+  },
+];
+
+export default itemData;
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, ImageList, ImageListItem, Link, Typography } from '@mui/material';
+import itemData from '../../assets/images';
 import './Portfolio.scss';
 
 const Portfolio = () => {
@@ -19,20 +20,35 @@ const Portfolio = () => {
         Наши работы
       </Typography>
       <Typography
-          component="span"
-          sx={{
-            color: '#000',
-            fontSize: '22px',
-            fontWeight: '300',
-            lineHeight: '1.5',
-            marginBottom: '60px',
-          }}
-          className="portfolio-text-subtitle"
-        >
-          Примеры выполненных работ, которыми гордится наша команда
-        </Typography>
+        component="span"
+        sx={{
+          color: '#000',
+          fontSize: '22px',
+          fontWeight: '300',
+          lineHeight: '1.5',
+          marginBottom: '60px',
+        }}
+        className="portfolio-text-subtitle"
+      >
+        Примеры выполненных работ, которыми гордится наша команда
+      </Typography>
+      <ImageList sx={{ width: 1200, height: 665 }} cols={4} rowHeight={330} gap={10}>
+        {itemData.map((item) => (
+          <Link>
+            <ImageListItem key={item.img} sx={{ height: '330px' }}>
+              <img
+                src={item.img}
+                srcSet={item.img}
+                alt={item.title}
+                loading="lazy"
+                className="portfolio-image-item"
+              />
+            </ImageListItem>
+          </Link>
+        ))}
+      </ImageList>
     </Box>
-  )
-}
+  );
+};
 
 export default Portfolio;
