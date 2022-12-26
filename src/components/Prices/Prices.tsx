161cfdf39aff0c5c6ce39ed317b550d94ac6d3c5
exekuta <exekuta@gmail.com>
2022-12-26 16:52:32 +0300

    feat: change input html tag to mui TextField component

--- src/components/Prices/Prices.tsx
+++ src/components/Prices/Prices.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import './Prices.scss';
 
 const Prices = () => {
@@ -33,27 +33,42 @@ const Prices = () => {
           расчет в течение 24 часов и свяжемся с вами
         </Typography>
       </Box>
-      <Box component="div">
-        <form className="prices-inputs">
-          <input type="text" placeholder="Имя" className='input-item'></input>
-          <input type="text" placeholder="Телефон" className='input-item'></input>
-          <input type="text" placeholder="e-mail" className='input-item'></input>
-          {/* <button type="submit" className='submit-button'>Отправить заявку</button> */}
-          <Button
-            variant="contained"
-            color="warning"
-            sx={{
-              color: '#fff',
-              textTransform: 'none',
-              height: '56px',
-              padding: '0 30px 0 30px',
-              fontWeight: '700',
-              fontSize: '14px',
-            }}
-          >
-            Отправить заявку
-          </Button>
-        </form>
+      <Box component="form" className="prices-inputs">
+        <TextField
+          className="input-item"
+          label="Имя" 
+          variant="outlined"
+          fullWidth
+          autoFocus={ true }
+        />
+        <TextField
+          className="input-item"
+          label="Телефон" 
+          variant="outlined"
+          fullWidth
+          autoFocus={ false }
+        />
+        <TextField
+          className="input-item"
+          label="e-mail" 
+          variant="outlined"
+          fullWidth
+          autoFocus={ false }
+        />
+        <Button
+          variant="contained"
+          color="warning"
+          sx={{
+            color: '#fff',
+            textTransform: 'none',
+            height: '56px',
+            padding: '0 30px 0 30px',
+            fontWeight: '700',
+            fontSize: '14px',
+          }}
+        >
+          Отправить заявку
+        </Button>
       </Box>
     </Box>
   );
